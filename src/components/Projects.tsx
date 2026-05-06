b970ba671697c4c51dd40f11fe340332c1087ac6
bredelsss <bernardinusodie5@gmail.com>
2026-05-06 19:43:39 +0700

    Update Projects.tsx

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -38,15 +38,6 @@ const projects = [
     image: "/Moodboard5.png",
     link: "https://www.figma.com/design/sqHoopNuey9VQ5bv45OkqV/Paceflux?node-id=0-1&t=67ULrL9fR1NPqn2N-1",
   },
-  {
-    id: "05",
-    title: "Develop Clothing Store Mobile Application Design Project",
-    role: "UI/UX Designer",
-    summary: "A sleek mobile app centered on clothing shopping (discovery, buying, customization), delivering an intuitive interface and striking visual design.,
-    tags: ["UI/UX", "Figma", "Wireframe", "Prototyping", "Design Systems"],
-    image: "/Moodboard4.png",
-    link: "https://www.figma.com/design/hO926bW8imf75T3LoGMzew/ProjectDummiest?node-id=263-359&t=zB8nf3ak1Cd64eS8-1",
-  },
 ];
 
 function ProjectCard({ project, index }: { project: typeof projects[0], index: number }) {
